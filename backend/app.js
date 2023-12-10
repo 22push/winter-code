f3039b971ab94e0e88cdffa43ebba1c5187df4d6
Rachit Jain <122090421+laughing-philosopher@users.noreply.github.com>
2023-12-10 16:55:05 +0530

    Update app.js
    added medicine router and its route

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 let cookieParser = require("cookie-parser");
 const usersignuprouter = require("./routes/signuproute");
 const app = express();
+const medicineRouter = require('./routes/medicineRoutes');
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -13,4 +14,5 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use("/user",usersignuprouter);
+app.use("/api/v1", medicineRouter);
 module.exports = app;
